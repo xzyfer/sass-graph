f01d52ade64195e55ecbb745fb63d0941a83b743
Chris Campbell <chris@harto.org>
2015-02-21 13:03:29 +1100

    Generalise parse imports test

--- test/test.parse-imports.js
+++ test/test.parse-imports.js
@@ -5,68 +5,68 @@ var path = require("path");
 describe('parse-imports', function () {
 
   it('should parse single import with single quotes', function () {
-    var scss = "@import 'app'; ";
-    var result = parseImports(scss);
+    var cssImport = "@import 'app'; ";
+    var result = parseImports(cssImport);
     assert.equal(result.length, 1);
     assert.equal(result[0], "app");
   });
 
   it('should parse single import with double quotes', function () {
-    var scss = '@import "app"; ';
-    var result = parseImports(scss);
+    var cssImport = '@import "app"; ';
+    var result = parseImports(cssImport);
     assert.equal(result.length, 1);
     assert.equal(result[0], "app");
   });
 
   it('should parse single import with extra spaces after import', function () {
-    var scss = '@import  "app"; ';
-    var result = parseImports(scss);
+    var cssImport = '@import  "app"; ';
+    var result = parseImports(cssImport);
     assert.equal(result.length, 1);
     assert.equal(result[0], "app");
   });
 
   it('should parse single import with extra spaces before ;', function () {
-    var scss = '@import "app" ; ';
+    var cssImport = '@import "app" ; ';
   });
 
   it('should parse two individual imports', function () {
-    var scss = '@import "app"; \n ' + 
+    var cssImport = '@import "app"; \n ' +
                '@import "foo"; \n';
-    var result = parseImports(scss);
+    var result = parseImports(cssImport);
     ["app", "foo"].forEach(function (dep) {
       assert.equal(result.length, 2);
       assert.notEqual(result.indexOf(dep), -1);
-    });       
+    });
   });
 
   it('should parse two imports on same line', function () {
-    var scss = '@import "app", "foo";';  
-    var result = parseImports(scss);
+    var cssImport = '@import "app", "foo";';
+    var result = parseImports(cssImport);
     ["app", "foo"].forEach(function (dep) {
       assert.equal(result.length, 2);
       assert.notEqual(result.indexOf(dep), -1);
-    });       
+    });
   });
 
   it('should parse two imports continued on multiple lines ', function () {
-    var scss = '@import "app", \n' +
-                   '"foo"; \n';  
-    var result = parseImports(scss);               
+    var cssImport = '@import "app", \n' +
+                   '"foo"; \n';
+    var result = parseImports(cssImport);
     ["app", "foo"].forEach(function (dep) {
       assert.equal(result.length, 2);
       assert.notEqual(result.indexOf(dep), -1);
-    });       
+    });
   });
 
   it('should parse three imports with mixed style ', function () {
-    var scss = '@import "app", \n' +
-                        '"foo";\n ' +  
+    var cssImport = '@import "app", \n' +
+                        '"foo";\n ' +
                '@import "bar";';
-    var result = parseImports(scss);
+    var result = parseImports(cssImport);
     ["app", "foo", "bar"].forEach(function (dep) {
       assert.equal(result.length, 3);
       assert.notEqual(result.indexOf(dep), -1);
-    });       
+    });
   });
 
 
