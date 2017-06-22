57f535ae98833eccdfeef1a7d2bcc8b33b536361
Marcel Robitaille <marcelrobitaille11@gmail.com>
2017-06-22 12:20:59 -0300

    Add test for import globbing

--- test/parse-file.js
+++ test/parse-file.js
@@ -160,5 +160,14 @@ describe('sass-graph', function(){
         });
       });
     });
+
+    describe('glob imports', function() {
+      it('should glob imports if options.globImports is truthy', function () {
+        graph({ globImports: true }).fromFixtureFile('glob-imports').assertDecendents([
+          path.join('nested', 'c.scss'),
+          'b.scss',
+        ]);
+      });
+    })
   });
 });
